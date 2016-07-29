6c24abe9f59a589c5927cbe5f6b0a32ba59c8934
Andrey Zakharov <a.zakharov3@gmail.com>
2016-07-28 20:40:37 -0700

    added async test

--- spec/AddressBookSpec.js
+++ spec/AddressBookSpec.js
@@ -1,8 +1,35 @@
 describe("AddressBook", function () {
+    var addrBook;
+    var contact;
+
+    beforeEach(function () {
+        addrBook = new AddressBook();
+        contact = new Contact();
+    });
+
     it("should be able to add a contact", function () {
-        var addrBook = new AddressBook();
-        var contact = new Contact();
         addrBook.addContact(contact);
         expect(addrBook.getContact(0)).toBe(contact);
+    });
+
+    it("should be able to delete a contact", function () {
+        addrBook.addContact(contact);
+        addrBook.deleteContact(0);
+        expect(addrBook.getContact(0)).not.toBeDefined();
+    });
+});
+
+describe("Async Address Book", function () {
+    var addressBook = new AddressBook();
+
+    beforeEach(function (done){
+        addressBook.getInitialContacts(function () {
+            done();
+        });
+    });
+
+    it("should grab initial contacts", function (done) {
+        expect(addressBook.initialComplete).toBe(true);
+        done();
     });
 });
--- src/AddressBook.js
+++ src/AddressBook.js
@@ -1,9 +1,25 @@
 var AddressBook = function () {
-    this.Contacts = [];
+    this.contacts = [];
+    this.initialComplete = false;
 };
-AddressBook.prototype.addContact = function(contact){
-    this.Contacts.push(contact);
+AddressBook.prototype.getInitialContacts = function (cb) {
+    var self = this;
+    setTimeout(function () {
+        self.initialComplete = true;
+        if (cb) {
+            return cb();
+        }
+    }, 3);
 }
-AddressBook.prototype.getContact = function(contactIndex){
-    return this.Contacts[contactIndex]
+
+AddressBook.prototype.addContact = function (contact) {
+    this.contacts.push(contact);
+};
+
+AddressBook.prototype.getContact = function (index) {
+    return this.contacts[index]
+};
+
+AddressBook.prototype.deleteContact = function(index){
+    this.contacts.splice(index,1);
 }
